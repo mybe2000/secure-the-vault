21a09201ee9f457f095dd7b0c0963111899a6ee8
Myha Kalaj <mybe2000@gmail.com>
2024-01-30 15:12:06 -0500

    added index.html pseudocode and script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// pseudocode
+// create var for number 1 with arithmetic operator "+"
+// create var for number 2 with arithmetic operator "-"
+// create var for number 3 with arithmetic operator "*"
+// alert with combination of the three numbers
+
+// create var for each number with an arithmetic operator
+const num1 = 20 - 10;
+const num2 = 25 + 15;
+const num3 = 13 * 3;
+
+// alert with message for user
+alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+);
+
+// alert with combination of the three numbers
+alert(num1 + " " + num2 + " " + num3);
